31338dcccb6856c291181283a279d410a4995ed1
m-wells <mwellsa@gmail.com>
2022-09-23 14:02:13 -0400

    Tables.jl interface

--- README.md
+++ README.md
@@ -1,10 +1,48 @@
 # LazyTables
 
-Exports `LazyTable`.
+Exports `LazyTable` and `LazyRow`.
 A simple wrapper around a `TypedTables.Table` that avoids allocation when iterating over rows.
 Can be constructed just like a `TypedTables.Table` (or can be called on a `TypedTables.Table` directly).
+A `LazyTable` returns a `LazyRow`.
+This should function as a drop in replacement for any `TypedTables.Table`.
 
 ## Examples
 
 ## Benchmark
 Use `KeplerLC`.
+
+```
+julia> using Kepler, KEBS
+
+julia> kebc = KEBC(); lc = KeplerLC(kebc[1]);
+
+julia> lz = getfield(lc, :table)
+LazyTable with 41 columns and 69302 rows:
+	...
+
+julia> tt = getfield(lz, :table);
+Table with 41 columns and 69302 rows:
+	...
+
+julia> f(x) = x.bkg[10]
+f (generic function with 1 method)
+
+julia> g(x) = x[10].bkg
+g (generic function with 1 method)
+
+julia> f(tt); @time f(tt)
+  0.000004 seconds (1 allocation: 16 bytes)
+0.06335759f0
+
+julia> g(tt); @time g(tt)
+  0.000042 seconds (244 allocations: 86.625 KiB)
+0.06335759f0
+
+julia> f(lz); @time f(lz)
+  0.000005 seconds (1 allocation: 16 bytes)
+0.06335759f0
+
+julia> g(lz); @time g(lz)
+  0.000004 seconds (1 allocation: 16 bytes)
+0.06335759f0
+```
--- src/LazyTables.jl
+++ src/LazyTables.jl
@@ -1,41 +1,62 @@
 module LazyTables
 
 export LazyTable
-import TypedTables
+export LazyRow
+import Tables
+import TypedTables: Table
 
 #===========================================================================================
 Private
 ===========================================================================================#
-_data(lz) = getfield(lz, :data)
-_index(lz) = getfield(lz, :index)
-_table(lz) = getfield(lz, :table)
+@inline _data(lz) = Core.getfield(lz, :data)
+@inline _index(lz) = Core.getfield(lz, :index)
+@inline _table(lz) = Core.getfield(lz, :table)
 
 #===========================================================================================
 Constructors
 ===========================================================================================#
-const Table{K, V} = TypedTables.Table{K, 1, V}
-
 struct LazyRow{K, V}
     data::NamedTuple{K, V}
     index::Int
 end
 
-struct LazyTable{K, V} <: AbstractVector{LazyRow{K, V}}
-    table::Table{K, V}
+struct LazyTable{K, N, V} <: AbstractArray{LazyRow{K, V}, N}
+    table::Table{K, N, V}
     data::V
-    function LazyTable(table::Table{K, V}) where {K, V}
-        return new{K, V}(table, _data(table))
+    function LazyTable(table::Table{K, N, V}) where {K, N, V}
+        return new{K, N, V}(table, _data(table))
     end
 end
 
-LazyTable(args...; kwargs...) = LazyTable(TypedTables.Table(args...; kwargs...))
+@inline LazyTable(args...; kwargs...) = LazyTable(Table(args...; kwargs...))
+
+Table(lz::LazyTable) = _table(lz)
+
+#===========================================================================================
+Tables Interface
+===========================================================================================#
+Tables.istable(::Type{<:LazyTable}) = true
+Tables.rowaccess(::Type{<:LazyTable}) = true
+Tables.columnaccess(::Type{<:LazyTable}) = true
+Tables.schema(::LazyTable{T}) where {T} = Tables.Schema(T)
+Tables.materializer(::LazyTable) = Table
+
+"""
+    columns(table::LazyTable)
+Convert a `LazyTable` into a `NamedTuple` of its columns.
+"""
+@inline Tables.columns(t::LazyTable) = _data(t)
+@inline Tables.rows(t::LazyTable) = t
 
 #===========================================================================================
 AbstractArray Interface
 ===========================================================================================#
-Base.size(lz::LazyTable) = size(_table(lz))
-Base.getindex(lz::LazyTable, i::Int) = LazyRow(_data(lz), i)
-Base.IndexStyle(::Type{<:LazyTable}) = IndexLinear()
+@inline Base.getindex(lz::LazyTable, i::Int) = LazyRow(_data(lz), i)
+
+# offload to TypedTables
+@inline Base.size(lz::LazyTable) = size(_table(lz))
+@inline Base.axes(lz::LazyTable) = axes(_table(lz))
+@inline Base.IndexStyle(lz::LazyTable) = IndexStyle(_table(lz))
 
 #===========================================================================================
 Properties
